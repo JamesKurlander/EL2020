84fd4f64ba074fd35b6c6be339f0c26276401edb
N03371595 <james.h.kurlander@gmail.com>
2020-05-12 22:56:02 -0400

    The last commit was a lie (I forgot to add the robo_buddy.py file). This is the second version of my final project. It is now capable of playing a .wav audio file and snapping an image when an intruder is detected. It then does the same thing as before and emails the user along with an attached image of the intruder.

--- final/robo_buddy.py
+++ final/robo_buddy.py
@@ -1,34 +1,68 @@
+#!/usr/bin/python
+
 import RPi.GPIO as GPIO
 import time
 import os
 import time
 import smtplib
 import socket
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+import picamera
 
 PIR_pin = 4
 eFROM = "james.h.kurlander@gmail.com"
 eTO = "james.h.kurlander@gmail.com"
 Subject = "Intruder Alert!"
 Text = "An intruder has been spotted! Go check it out!"
-server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+server = smtplib.SMTP('smtp.gmail.com', 587)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(PIR_pin, GPIO.IN)
 
+#runs indefinitely, checking the area for any movement
 def detect():
 	for i in range(101):
 		if GPIO.input(PIR_pin):
 			print("Entity detected!")
-			alert()
+			from subprocess import call
+			call(["aplay", "/home/james/EL2020/final/audio_files/actions_have_consequences.wav"])
+			capture()
+			time.sleep(60)
 		else:
 			print("Nothing detected...")
 		time.sleep(2)
 
-def alert():
-	eMessage = 'Subject: {}\n\n{}'.format(Subject, Text)
+#captures an image of the intruder when called
+def capture():
+	with picamera.PiCamera() as camera:
+		camera.resolution = (1024, 768)
+		camera.start_preview()
+		time.sleep(2)
+		camera.capture('intruder.jpg')
+		alert('intruder.jpg')
+
+#takes in an image and alerts the owner through an email with an image of the intruder
+def alert(img_file):
+	img_data = open(img_file, 'rb').read()
+	msg = MIMEMultipart()
+	msg['Subject'] = 'INTRUDER ALERT!'
+	msg['From'] = eFROM
+	msg['To'] = eTO
+
+	text = MIMEText("Here's a photo of the perpetrator! Go check it out!")
+	msg.attach(text)
+	image = MIMEImage(img_data, name=os.path.basename(img_file))
+	msg.attach(image)
+
+	server.set_debuglevel(1)
+	server.ehlo()
+	server.starttls()
+	server.ehlo()
 	server.login(eFROM, "twxqclibkkepzisu")
-	server.sendmail(eFROM, eTO, eMessage)
-	server.quit
+	server.sendmail(eFROM, eTO, msg.as_string())
+	server.quit()
 
 detect()
 GPIO.cleanup()
